773a9d12ca86c461917ec349519b89ea402031df
MUHAMMAD OSAMA <developerscripters@gmail.com>
2024-09-02 06:04:43 -0700

    second

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,8 @@
 'use client';
 
 import { useState } from 'react';
+import { FaPaperclip, FaPaperPlane } from 'react-icons/fa';
+import { FiSmile } from 'react-icons/fi';
 
 const ChatApp = () => {
   const [messages, setMessages] = useState([
@@ -20,7 +22,7 @@ const ChatApp = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen  mx-96">
+    <div className="flex flex-col h-screen mx-96">
       {/* Chat Header */}
       <div className="bg-white p-4 flex items-center shadow-md">
         <img
@@ -35,68 +37,83 @@ const ChatApp = () => {
         <div className="mt-6 ml-6 text-sm text-red-500">Deadline: 30-Aug-2024</div>
       </div>
 
-      
-      <div className='border-gray-300 rounded-xl border-2 h-2/3'>
+      {/* Container for Messages and Input */}
+      <div className="flex flex-col flex-grow h-4/5 mb-6 border-gray-300 rounded-xl border-2">
         {/* Message List */}
-      <div className="flex-1 overflow-y-auto p-4">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`flex ${
-              message.sender === 'user' ? 'justify-end' : 'justify-start'
-            } mb-4`}
-          >
-            <div className="flex items-end max-w-xs">
-              {/* Conditional rendering for the sales member image */}
-              {message.sender === 'salesmember' && (
-                <img
-                  src="https://cdn.esquimaltmfrc.com/wp-content/uploads/2015/09/flat-faces-icons-circle-man-9.png" 
-                  alt="salesmember"
-                  className="w-8 h-8 rounded-full mr-2"
-                />
-              )}
-              
-              <div
-                className={`p-3 rounded-lg ${
-                  message.sender === 'user'
-                    ? 'border-blue-500 border-2 text-black'
-                    : 'border-orange-300 border-2 text-black'
-                }`}
-              >
-                {message.text}
+        <div className="flex-1 overflow-y-auto p-4">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`flex ${
+                message.sender === 'user' ? 'justify-end' : 'justify-start'
+              } mb-4`}
+            >
+              <div className="flex items-end max-w-xs">
+                {/* Conditional rendering for the sales member image */}
+                {message.sender === 'salesmember' && (
+                  <img
+                    src="https://cdn.esquimaltmfrc.com/wp-content/uploads/2015/09/flat-faces-icons-circle-man-9.png" 
+                    alt="salesmember"
+                    className="w-8 h-8 rounded-full mr-2"
+                  />
+                )}
+                
+                <div
+                  className={`p-3 rounded-lg ${
+                    message.sender === 'user'
+                      ? 'border-blue-500 border-2 text-black'
+                      : 'border-orange-300 border-2 text-black'
+                  }`}
+                >
+                  {message.text}
+                </div>
+                
+                {message.sender === 'user' && (
+                  <img
+                    src="/imgU.png" 
+                    alt="user"
+                    className="w-8 h-8 rounded-full ml-2"
+                  />
+                )}
               </div>
-              
-              
-              {message.sender === 'user' && (
-                <img
-                  src="/imgU.png" 
-                  alt="user"
-                  className="w-8 h-8 rounded-full ml-2"
-                />
-              )}
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      {/* Message Input */}
-      <div className="p-4 bg-white shadow-md flex items-center">
-        <button className="text-blue-600 mr-4 text-2xl">+</button>
-        <input
-          type="text"
-          placeholder="Type your message..."
-          className="flex-1 border border-gray-300 rounded-lg p-2 mr-2"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
-        />
-        <button
-          className="bg-blue-600 text-white rounded-lg px-4 py-2"
-          onClick={sendMessage}
-        >
-          Send
-        </button>
-      </div>
+        {/* Message Input */}
+        <div className="p-4 bg-white shadow-md flex items-center">
+          <div className="relative flex-1">
+
+
+            <input
+              type="text"
+              placeholder="Type your message..."
+              className="w-full border border-gray-300 rounded-lg p-2 pr-12"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+            />
+
+            {/* Emoji Icon */}
+            <button className="absolute right-14 top-1/2 transform -translate-y-1/2 text-blue-500 rounded-lg px-4 py-1">
+              <FiSmile size={24} /> {/* Using an emoji smile icon */}
+            </button>
+
+             {/* Paperclip Icon */}
+             <button className="absolute right-8 top-1/2 transform -translate-y-1/2 text-blue-500 rounded-lg px-4 py-1">
+              <FaPaperclip /> {/* Paperclip icon */}
+            </button>
+            
+            <button
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-500 rounded-lg px-4 py-1"
+              onClick={sendMessage}
+            >
+
+              
+              <FaPaperPlane />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
